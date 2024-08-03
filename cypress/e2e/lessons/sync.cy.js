38195a6a29c7a979f63f63741a651d7837f55834
AnaStadelhofer <ana.stadelhofer@outlook.com>
2024-08-03 16:37:47 -0300

    coomit

--- cypress/e2e/lessons/sync.cy.js
+++ cypress/e2e/lessons/sync.cy.js
@@ -1,12 +1,8 @@
 /// <reference types="cypress"/>
 
 describe("Sync", () => {
-
-    before(() => { //vai executar antes do primeiro  teste
-        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
-    });
     
-    beforeEach(() => { //vai executar antes de cada um dos tests
+    beforeEach(() => {
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
     });
 
@@ -27,4 +23,67 @@ describe("Sync", () => {
          .type('Campo ficou visível')
     })
 
+    it("Refazer tentativas", () => {
+
+        cy.get('#novoCampo')
+            .should('not.exist')
+
+        cy.get('#buttonDelay')
+            .click()
+        
+        cy.get('#novoCampo')
+            .should('not.exist')
+
+        cy.get('#novoCampo')
+            .should('exist')
+            .type('Preenchendo campo')
+    })
+
+    it("Usando o find", () => {
+
+        cy.get('#buttonList')
+            .click()
+
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 2')
+    })
+
+    it("Alterando o timeout dos testes", () => {
+
+        cy.get('#buttonDelay')
+            .click()
+
+        cy.get('#novoCampo', { timeout: 1000 })
+            .should('exist')
+    })
+
+    it("Usando o wait", () => {
+
+        cy.get('#buttonListDOM')
+            .click()
+
+            cy.wait(10000)
+
+            cy.get('#lista li span')
+                .should('have.length', 2)
+    })
+
+    it("Retry do Clcik", () => {
+      cy.get("#buttonCount")
+        .click()
+        .click()
+        .should('have.value', '111')  
+    })
+
+    it.only("Then vs Should", () => {
+        cy.get("#buttonListDOM").then($el => {
+            expect($el).to.have.length(1)
+            console.log($el)
+        })
+    })
 })
